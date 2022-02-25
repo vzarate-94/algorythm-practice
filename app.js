384c586a90f50f3e31ce85b370874cc7de5f2427
vzarate26 <vzarate26@gmail.com>
2022-02-24 22:36:29 -0500

    solve problem fromPairs to be O(n) linear

--- app.js
+++ app.js
@@ -210,4 +210,26 @@ function mumble(str) {
   return output.slice(0, -1).join("")
 }
 
-console.log(mumble('abc'))
+
+// Difficulty: Intermediate
+
+// Prompt:
+
+// - Write a function named fromPairs that creates an object from an array containing nested arrays.
+// - Each nested array will have two elements representing key/value pairs used to create key/value pairs in an object to be returned by the function.
+// - If a key appears in multiple pairs, the rightmost pair should overwrite the previous entry in the object.
+
+// Examples:
+
+// fromPairs([ ['a', 1], ['b', 2], ['c', 3] ]) //=> { a: 1, b: 2, c: 3 }
+// fromPairs([ ['name', 'Sam"], ['age', 24], ['name', 'Sally'] ]) //=> { name: "Sally", age: 24 }
+
+function fromPairs(arr) {
+  let obj = {}
+  arr.forEach(function(keyValuePair) {
+    obj[keyValuePair[0]] = [keyValuePair[1]]
+  });
+  return obj
+}
+
+console.log(fromPairs([['a', 1], ['b', 2], ['c', 3] ]))
